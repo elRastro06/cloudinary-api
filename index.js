2a56be19f7fe49cdf9cf43ac84564ab31f645ea4
JCamacho4 <jorgecamagarc@gmail.com>
2023-11-08 11:45:33 +0100

    upload images files with overwriting to cloudinary

--- index.js
+++ index.js
@@ -1,36 +1,52 @@
 const axios = require('axios');
 const express = require('express');
 const cloudinary = require('./cloudinary');
+const multer = require("multer");
+
+const storage = multer.memoryStorage(); // Puedes cambiarlo según tus necesidades
+const upload = multer({ storage: storage });
+
 
 // CONFIGURE EXPRESS
 const app = express();
-const port = 5000;
+const port = 5004   ;
 
-app.use(express.json());
+
+// use in app x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
+
 
 app.listen(port, () => {
     console.log(`Now listening on port ${port}`);
 });
 
 
+
 // UPLOAD IMAGE
-const image = 'image_test/gato.jpg';
-const upload_preset = process.env.UPLOAD_PRESET;
 
-app.post('/upload', async (req, res) => {
-    const productName = req.body.productName;
-    const imageName = req.body.imageName;
+const upload_preset_signed = process.env.UPLOAD_PRESET_SIGNED;
+const upload_preset_unsigned = process.env.UPLOAD_PRESET_UNSIGNED    ;
 
-    if(req.body.imageName && req.body.productName){
+app.post('/upload', upload.single('image'), (req, res) => {
+
+    const { productName, imageName } = req.body;
+    const imageBinaryData = req.file;    
+
+    console.log(productName  + " " + imageName);
+
+    if(imageName && productName && imageBinaryData){
+        
+        const imageBase64 = req.file.buffer.toString('base64');
 
         const options = {
             folder: productName,
             public_id: imageName,
+            upload_preset: upload_preset_signed,
+            overwrite: true
         };
     
-        cloudinary.uploader.unsigned_upload(image, upload_preset, options).then((result) => {
-            console.log(result);
+        cloudinary.uploader.upload('data:image/png;base64,' + imageBase64, options).then((result) => {
+            console.log("success upload");
             res.json({
                 message: "success",
                 secure_url: result.secure_url,
@@ -46,5 +62,3 @@ app.post('/upload', async (req, res) => {
     }
 
 });
-
-
