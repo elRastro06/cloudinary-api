0464f763cb91507525ffe3e3280fe03288fc62e8
JCamacho4 <jorgecamachog@uma.es>
2023-12-26 12:28:29 +0100

    return to good version

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const v1 = require('./v1.js');
+const v2 = require('./v2.js');
+const cors = require('cors');
+
+
+// CONFIGURE EXPRESS
+const app = express();
+const port = 5004;
+
+app.use(cors());
+// use in app x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.listen(port, () => {
+    console.log(`Now listening on port ${port}`);
+});
+
+app.use("/v1", v1);
+app.use("/v2", v2);
